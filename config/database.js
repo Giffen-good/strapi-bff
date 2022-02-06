0cbec5274b117d5ad36d442c3e172f7513b0b0e1
Giffen-good <chris6rock@gmail.com>
2022-02-06 12:43:17 -0500

    to prod

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'bff_postgres'),
+        username: env('DATABASE_USERNAME', 'bff_super_user'),
+        password: env('DATABASE_PASSWORD', 'use and value diversity'),
+        schema: env("DATABASE_SCHEMA", "public"),
     },
-    useNullAsDefault: true,
-  },
+  }
 });
