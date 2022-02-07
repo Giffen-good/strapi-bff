ed4603f7041f4737a1cd4ac6c29c1009364b0d07
Giffen-good <chris6rock@gmail.com>
2022-02-07 13:28:41 -0500

    deploy to heroku

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,13 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET
     },
     actionOptions: {
       upload: {},
