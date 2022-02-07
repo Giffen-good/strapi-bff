bc0fe40e2b5b183094293865adcd179f4ce6c8ba
Giffen-good <chris6rock@gmail.com>
2022-02-07 14:03:03 -0500

    splo

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,12 +1,9 @@
 module.exports = ({ env }) => ({
   host: "0.0.0.0",
-  url: env("https://giffen-good-strapi-deploys.herokuapp.com"),
+  url: env("RENDER_EXTERNAL_URL"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
     },
-  },
-  app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '02c8ae6aa94de991b9df3d7be1b2dd90'),
+    },
   },
 });
