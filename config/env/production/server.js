ef0574c19be4bef585c8af008926104bf6f11af2
Giffen-good <chris6rock@gmail.com>
2022-02-06 19:42:00 -0500

    deploy to render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
   host: "0.0.0.0",
-  url: env("RENDER_EXTERNAL_URL"),
+  url: env("https://strapi-1q6e.onrender.com"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
     },
+  },
+  app: {
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
