0a7fa8c5f7d4caeaa445dddc44a1b6f33b42ef40
Giffen-good <chris6rock@gmail.com>
2022-02-07 13:02:03 -0500

    deploy to heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: "0.0.0.0",
-  url: env("https://strapi-1q6e.onrender.com"),
+  url: env("https://giffen-good-strapi-deploys.herokuapp.com"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
