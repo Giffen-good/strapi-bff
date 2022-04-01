5d289c56d52967f06f3146190fec1a4bb330ce30
Giffen-good <chris6rock@gmail.com>
2022-04-01 04:09:15 -0400

    discover

---
+++ src/api/shopify-designer/controllers/shopify-designer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  shopify-designer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::shopify-designer.shopify-designer');
---
+++ src/api/shopify-designer/services/shopify-designer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shopify-designer service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::shopify-designer.shopify-designer');
